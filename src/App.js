40d8ca8868213523b906511ff397a4c7d96e8809
Nic Kendall <nic.kendall.dev@proton.me>
2022-08-10 14:15:16 -0400

    7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import "./App.css";
 import Home from "./Components/home";
 import Navbar from "./Components/navbar";
 
 function App() {
-  const title = "Welcome to the new blog!";
-  const likes = 50;
-
   return (
     <div className="App">
       <Navbar />
--- src/Components/home.js
+++ src/Components/home.js
@@ -1,8 +1,15 @@
 const Home = () => {
-    return (
-        <div className="home"><h2>Homepage</h2></div>
-      );
-}
- 
+  const handleClick = (e) => console.log("Hello Ninjas", e);
+  const handleClick2 = (name, e) => console.log("Hello " + name, e);
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={(e) => handleClick2("Bobby", e)}>Click Me Again</button>
+    </div>
+  );
+};
+
 export default Home;
-<div className="home"></div>
+<div className="home"></div>;
