2e4cf61be9c3e3da47f4ca8c64c9da1889301cd3
Nic Kendall <nic.kendall.dev@proton.me>
2022-08-16 11:46:45 -0400

    20

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,29 +1,9 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          if (!res.ok) throw Error("Could not access resource.");
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsLoading(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setError(err.message);
-          setIsLoading(false);
-        });
-    }, 1000);
-  }, []);
+  const { data: blogs, isLoading, error } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
---
+++ src/Components/useFetch.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+        .then((res) => {
+          if (!res.ok) throw Error("Could not access resource.");
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setIsLoading(false);
+        });
+    }, 1000);
+  }, [url]);
+
+  return { data, isLoading, error }
+};
+
+export default useFetch;
